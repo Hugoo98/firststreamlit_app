cf50de11c0b4cf8649161a9ddf9b2ff355ad911b
Omar Tachour <56323269+Hugoo98@users.noreply.github.com>
2023-11-14 13:39:49 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ try:
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
 
-streamlit.write('The user entered ', fruit_choice)
+#streamlit.write('The user entered ', fruit_choice)
 
 
 #Don't run anything past here while we troubleshoot 
