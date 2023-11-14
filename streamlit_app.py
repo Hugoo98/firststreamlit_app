e6f5def0a59bd9184bd46c015e91e577b8999b91
Omar Tachour <56323269+Hugoo98@users.noreply.github.com>
2023-11-14 14:21:46 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,8 +63,6 @@ if streamlit.button('Get fruit Load List'):
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
     
-streamlit.stop()
-
 #####################################################
 
 def insert_row_snowflake(new_fruit):
@@ -77,7 +75,7 @@ if streamlit.button('Add a Fruit to the List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(add_my_fruit)
     streamlit.text(back_from_function)
-
+streamlit.stop()
 
 #This will note work correctly, but just go with it now
 #my_cur.execute("insert into fruit_load_list values('from streamlit')")
